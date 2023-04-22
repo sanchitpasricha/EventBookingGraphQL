61f03714e1824c8296548b40697029c68fc6c3e1
sanchitpasricha <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>
2023-04-22 21:56:36 +0530

    setting up mongo connection

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const graphqlHttp = require("express-graphql").graphqlHTTP;
 const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -64,6 +65,15 @@ app.use(
   })
 );
 
-app.listen(3000, () => {
-  console.log("Server running");
-});
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.sda1shk.mongodb.net/${process.env.MONGO_DB}`
+  )
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running & DB connected");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
